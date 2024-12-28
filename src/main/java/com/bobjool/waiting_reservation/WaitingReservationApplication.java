d21873ebe9e542152e31fdb1302f85253dd772f4
Griotold <gotjd9773@naver.com>
2024-12-28 13:16:15 +0900

    spring first commit

---
+++ src/main/java/com/bobjool/waiting_reservation/WaitingReservationApplication.java
@@ -0,0 +1,13 @@
+package com.bobjool.waiting_reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaitingReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaitingReservationApplication.class, args);
+	}
+
+}
