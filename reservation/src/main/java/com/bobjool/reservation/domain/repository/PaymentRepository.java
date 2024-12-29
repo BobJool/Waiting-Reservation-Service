13bd72e71ade9e22a99cf845407f4f161d46f6d4
Griotold <gotjd9773@naver.com>
2024-12-29 16:46:56 +0900

    [Payment] payment-search - repository, querydsl #1

--- reservation/src/main/java/com/bobjool/reservation/domain/repository/PaymentRepository.java
+++ reservation/src/main/java/com/bobjool/reservation/domain/repository/PaymentRepository.java
@@ -1,7 +1,11 @@
 package com.bobjool.reservation.domain.repository;
 
 import com.bobjool.reservation.domain.entity.Payment;
+import com.bobjool.reservation.domain.enums.PaymentStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -12,5 +16,13 @@ public interface PaymentRepository {
 
     Payment save(Payment payment);
 
+    // todo JpaRepository 에 있는 시그니쳐와 정확하게 동일해야 합니다.
     <S extends Payment> List<S> saveAll(Iterable<S> entities);
+
+    // todo 직접 만든 메서드는 테스트 해봐야 합니다.
+    Page<Payment> search(Long userId,
+                         PaymentStatus status,
+                         LocalDate startDate,
+                         LocalDate endDate,
+                         Pageable pageable);
 }
--- reservation/src/main/java/com/bobjool/reservation/infra/repository/payment/PaymentRepositoryCustom.java
+++ reservation/src/main/java/com/bobjool/reservation/infra/repository/payment/PaymentRepositoryCustom.java
@@ -1,7 +1,20 @@
 package com.bobjool.reservation.infra.repository.payment;
 
+import com.bobjool.reservation.domain.entity.Payment;
+import com.bobjool.reservation.domain.enums.PaymentStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.time.LocalDate;
+
 /**
  * querydsl 을 위한 인터페이스
  * */
 public interface PaymentRepositoryCustom {
+
+    Page<Payment> search(Long userId,
+                         PaymentStatus status,
+                         LocalDate startDate,
+                         LocalDate endDate,
+                         Pageable pageable);
 }
