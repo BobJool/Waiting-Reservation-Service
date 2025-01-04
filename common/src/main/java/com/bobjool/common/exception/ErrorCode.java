6c236d21d344188359ac98e19d70fc17a2e81171
Yeonju Seo <yeonjuseo712@gmail.com>
2025-01-04 11:45:34 +0900

    [Queue] fix: 에러코드 추가 #22

--- common/src/main/java/com/bobjool/common/exception/ErrorCode.java
+++ common/src/main/java/com/bobjool/common/exception/ErrorCode.java
@@ -33,13 +33,18 @@ public enum ErrorCode {
     DUPLICATED_NAME(HttpStatus.BAD_REQUEST,"식당 이름이 이미 존재합니다."),
     DUPLICATED_PHONE(HttpStatus.BAD_REQUEST, "식당 연락처가 이미 존재합니다."),
     DUPPLICATED_Address(HttpStatus.BAD_REQUEST,"식당 주소가 이미 존재합니다."),
+
     // 대기열
     USER_ALREADY_IN_QUEUE(HttpStatus.CONFLICT, "사용자가 이미 다른 대기열에 등록되어 있습니다."),
     QUEUE_PUBLISHING_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "대기열 등록 발행에 실패했습니다."),
     UNSUPPORTED_DINING_OPTION(HttpStatus.BAD_REQUEST, "지원하지 않는 식당 이용 방식입니다."),
     UNSUPPORTED_QUEUE_STATUS(HttpStatus.BAD_REQUEST, "지원하지 않는 줄서기 상태입니다."),
     UNSUPPORTED_QUEUE_TYPE(HttpStatus.BAD_REQUEST, "지원하지 않는 줄서기 방식입니다."),
+
     // 예약
+    INVALID_GUEST_COUNT(HttpStatus.BAD_REQUEST, "예약 인원수는 양수여야 합니다."),
+    UNSUPPORTED_RESERVATION_STATUS(HttpStatus.BAD_REQUEST, "지원하지 않는 예약 상태입니다."),
+    CANNOT_CANCEL(HttpStatus.CONFLICT, "취소할 수 없는 예약 상태입니다."),
 
     // 결제
     UNSUPPORTED_PAYMENT_METHOD(HttpStatus.BAD_REQUEST, "지원하지 않는 결제 방식입니다."),
@@ -57,7 +62,6 @@ public enum ErrorCode {
     UNSUPPORTED_NOTIFICATION_TYPE(HttpStatus.BAD_REQUEST, "지원하지 않는 알림 종류 입니다."),
     INVALID_NOTIFICATION_STATUS(HttpStatus.BAD_REQUEST, "유효하지 않은 알림 상태 입니다.")
     // AI
-
 
     ;
 
