d10e090269dc6a2ab6f39853ddaac8f58c0be73a
JISU-YANG <jisuyang@egoist.im>
2025-01-06 17:47:27 +0900

    [Notification] feat: Slack Id 조회 기능 구현 #62

---
+++ notification/src/main/java/com/bobjool/notification/presentation/controller/SlackController.java
@@ -0,0 +1,29 @@
+package com.bobjool.notification.presentation.controller;
+
+import com.bobjool.common.presentation.ApiResponse;
+import com.bobjool.common.presentation.SuccessCode;
+import com.bobjool.notification.application.service.SlackService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/slack")
+@RequiredArgsConstructor
+public class SlackController {
+    private final SlackService slackService;
+
+    @GetMapping("/users")
+    public ResponseEntity<ApiResponse<Map<String, String>>> findSlackIdByEmail(@RequestParam("email") String email) {
+        String slackId = slackService.usersLookupByEmail(email);
+        return ApiResponse.success(
+                SuccessCode.SUCCESS,
+                Map.of("slack_id", slackId)
+        );
+    }
+}
