dd62386209e91567f515c8c2a165c2f9b8d1570a
Griotold <gotjd9773@naver.com>
2025-01-06 20:40:34 +0900

    [Payment] payment-service 로 분리작업 후 API 호출

--- payment/src/main/java/com/bobjool/payment/presentation/dto/PaymentCreateReqDto.java
+++ payment/src/main/java/com/bobjool/payment/presentation/dto/PaymentCreateReqDto.java
@@ -1,5 +1,6 @@
 package com.bobjool.payment.presentation.dto;
 
+import com.bobjool.payment.application.dto.PaymentCreateDto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
