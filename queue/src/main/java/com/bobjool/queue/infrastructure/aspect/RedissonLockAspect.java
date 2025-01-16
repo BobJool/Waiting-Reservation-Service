e40ad9a0b9499834c17586dae5cdf6ea5f9a7f50
Yeonju Seo <yeonjuseo712@gmail.com>
2025-01-16 21:30:24 +0900

    [Queue] fix: 분산락 어노테이션 구현 적용 및 보상트랜젝션 적용 #94

--- queue/src/main/java/com/bobjool/queue/infrastructure/aspect/RedissonLockAspect.java
+++ queue/src/main/java/com/bobjool/queue/infrastructure/aspect/RedissonLockAspect.java
@@ -1,20 +1,17 @@
 package com.bobjool.queue.infrastructure.aspect;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import java.lang.reflect.Method;
+
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
-import org.springframework.expression.ExpressionParser;
-import org.springframework.expression.spel.standard.SpelExpressionParser;
-import org.springframework.expression.spel.support.StandardEvaluationContext;
 import org.springframework.stereotype.Component;
 
-import java.lang.reflect.Method;
-import java.util.concurrent.TimeUnit;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Aspect
@@ -22,45 +19,39 @@ import java.util.concurrent.TimeUnit;
 @RequiredArgsConstructor
 public class RedissonLockAspect {
 
+	private static final String REDISSON_LOCK_PREFIX = "LOCK:";
+
 	private final RedissonClient redissonClient;
+	private final AopForTransaction aopForTransaction;
 
 	@Around("@annotation(com.bobjool.queue.infrastructure.aspect.RedissonLock)")
-	public Object redissonLock(ProceedingJoinPoint joinPoint) throws Throwable {
-		MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+	public Object redissonLock(final ProceedingJoinPoint joinPoint) throws Throwable {
+		MethodSignature signature = (MethodSignature)joinPoint.getSignature();
 		Method method = signature.getMethod();
 		RedissonLock annotation = method.getAnnotation(RedissonLock.class);
 
-		// SpEL 표현식으로 복합 락 키 생성 (prefix 추가)
-		String lockKey = annotation.prefix() + generateLockKey(signature.getParameterNames(), joinPoint.getArgs(), annotation.value());
+		String lockKey = REDISSON_LOCK_PREFIX + CustomSpringELParser.getDynamicValue(signature.getParameterNames(),
+			joinPoint.getArgs(), annotation.value());
 		RLock lock = redissonClient.getLock(lockKey);
 
 		try {
-			if (lock.tryLock(annotation.waitTime(), annotation.leaseTime(), TimeUnit.MILLISECONDS)) {
-				log.info("Lock acquired for key: {}", lockKey);
-				return joinPoint.proceed();
-			} else {
-				log.warn("Failed to acquire lock for key: {}", lockKey);
-				throw new IllegalStateException("Unable to acquire lock for key: " + lockKey);
+			boolean available = lock.tryLock(annotation.waitTime(), annotation.leaseTime(), annotation.timeUnit());
+			log.info("RedissonLockAspect Lock acquired for key: {}", lockKey);
+			if (!available) {
+				return false;
 			}
+			return aopForTransaction.proceed(joinPoint);
+		} catch (InterruptedException e) {
+			log.warn("RedissonLockAspect Failed to acquire lock for key: {}", lockKey);
+			throw new IllegalStateException("Unable to acquire lock for key: " + lockKey);
 		} finally {
-			if (lock.isHeldByCurrentThread()) {
+			try {
 				lock.unlock();
-				log.info("Lock released for key: {}", lockKey);
+				log.info("RedissonLockAspect Lock released for key: {}", lockKey);
+			} catch (IllegalStateException e) {
+				log.info("RedissonLockAspect Lock Already UnLock: {} {}", method.getName(), lockKey);
 			}
 		}
 	}
 
-	private String generateLockKey(String[] paramNames, Object[] args, String expression) {
-		// SpEL 파서를 사용하여 동적 키 생성
-		ExpressionParser parser = new SpelExpressionParser();
-		StandardEvaluationContext context = new StandardEvaluationContext();
-
-		// 메서드 파라미터를 SpEL 컨텍스트에 설정
-		for (int i = 0; i < paramNames.length; i++) {
-			context.setVariable(paramNames[i], args[i]);
-		}
-
-		// SpEL 표현식 평가 및 결과 반환
-		return parser.parseExpression(expression).getValue(context, String.class);
-	}
 }
